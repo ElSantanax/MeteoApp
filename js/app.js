17c704a5b46147c74e8602eb0ca266b622bf2128
ElSantanax <josesantanaserrano130598@gmail.com>
2025-07-17 08:46:10 -0500

    Validación de formulario

--- js/app.js
+++ js/app.js
@@ -9,10 +9,34 @@ window.addEventListener('load', () => {
 function buscarClima(e) {
     e.preventDefault();
 
-    // Validar
+    const ciudad = document.querySelector('#ciudad').value;
+    const pais = document.querySelector('#pais').value;
 
+    if (ciudad === '' || pais === '') {
+        mostrarMensaje('Todos los campos son obligatorios.');
+        return;
+    }
 
     // Consultar API
 
-    
+}
+
+function mostrarMensaje(mensaje) {
+
+    const alertaError = document.querySelector('.bg-red-100');
+
+    if (!alertaError) {
+        const alerta = document.createElement('DIV');
+
+        alerta.classList.add('bg-red-100', 'border-red-400', 'text-red-700', 'px-4', 'py-3', 'rounded', 'max-w-md', 'mx-auto', 'mt-6', 'text-center',);
+        alerta.innerHTML = `
+            <strong class="font-bold">Error!</strong>
+            <span class="block">${mensaje}</span>
+        `
+        container.appendChild(alerta);
+
+        setTimeout(() => {
+            alerta.remove();
+        }, 3000);
+    }
 }
